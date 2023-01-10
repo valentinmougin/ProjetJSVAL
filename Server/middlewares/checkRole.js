616aad3b190c06eb5c598afbc8d7024a531b5211
valentinmougin <valentin.mougin@gmail.com>
2023-01-10 17:07:05 +0100

    commit

---
+++ Server/middlewares/checkRole.js
@@ -0,0 +1,24 @@
+const ForbiddenError = require("../errors/ForbiddenError");
+
+const ROLE = {
+  USER: 0,
+  ADMIN: 1,
+  SELLER: 2,
+  VISITOR: 3,
+};
+
+function checkRole({ minRole }) {
+  return function checkRoleMiddleware(req, res, next) {
+    const userRole = req.user.role;
+    console.log(userRole, minRole);
+    if (ROLES[userRole] >= minRole) {
+      next();
+    } else {
+      throw new ForbiddenError();
+    }
+  };
+}
+
+checkRole.ROLES = ROLES;
+
+module.exports = checkRole;
