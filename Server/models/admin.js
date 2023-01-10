3a44e767cfa1daf7fa5da646bc5005545ad62b1d
valentinmougin <valentin.mougin@gmail.com>
2023-01-10 17:46:18 +0100

    l

---
+++ Server/models/admin.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require("sequelize");
+const connection = require("./connection");
+const bcrypt = require("bcryptjs");
+class Admin extends Model {}
+
+Admin.init(
+  {
+    lastname: DataTypes.STRING,
+    firstname: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "USER",
+      validate: {
+        isIn: ["Admin"],
+      },
+    },
+  },
+  {
+    sequelize: connection,
+  }
+);
+
+Admin.addHook("beforeCreate", async (Admin) => {
+    Admin.password = await bcrypt.hash(Admin.password, await bcrypt.genSalt());
+});
+Admin.addHook("beforeUpdate", async (Admin, { fields }) => {
+  if (fields.includes("password")) {
+    Admin.password = await bcrypt.hash(Admin.password, await bcrypt.genSalt());
+  }
+});
+
+module.exports = Admin;
---
+++ Server/models/seller.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require("sequelize");
+const connection = require("./connection");
+const bcrypt = require("bcryptjs");
+class Seller extends Model {}
+
+Seller.init(
+  {
+    lastname: DataTypes.STRING,
+    firstname: DataTypes.STRING,
+    
+    email: {type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {isEmail: true,},
+    },
+    password: {type: DataTypes.STRING,
+      allowNull: false,
+      validate: {len: [1],},
+    },
+    role: {type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "USER",
+      validate: {isIn: ["Seller"],},
+    },
+  },
+  {
+    sequelize: connection,
+  }
+);
+
+Seller.addHook("beforeCreate", async (Seller) => {
+    Seller.password = await bcrypt.hash(Seller.password, await bcrypt.genSalt());
+});
+Seller.addHook("beforeUpdate", async (Seller, { fields }) => {
+  if (fields.includes("password")) {
+    Seller.password = await bcrypt.hash(Seller.password, await bcrypt.genSalt());
+  }
+});
+
+module.exports = Seller;
