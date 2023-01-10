75b9ffed5e7ac0eab6e91eec8173fb5be38e216f
valentinmougin <valentin.mougin@gmail.com>
2023-01-10 14:40:27 +0100

    Id

--- Server/index.js
+++ Server/index.js
@@ -11,5 +11,16 @@ app.post('/hello', (req,res)=>{
     res.json("Méthode POST");
 });
 
+app.put('/hello/:id', (req,res) =>{
+    const id = req.params.id;
+    res.json("Méthode PUT" + id, destinataire id,);
+});
+
+app.delete('/hello/:id', (req,res) =>{
+    const id = req.params.id;
+    res.json("Méthode delete" + id, destinataire id,);
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server is listening on port " + PORT));
